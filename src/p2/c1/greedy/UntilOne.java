9cd5ea4fcfa0672c74649fb1dca1304b541bebd6
cares0 <cares00000@gmail.com>
2022-04-24 19:18:51 +0900

    2-1. 그리디 - 1이 될 때까지(개선)
    알고리즘 개선

--- src/p2/c1/greedy/UntilOne.java
+++ src/p2/c1/greedy/UntilOne.java
@@ -22,15 +22,24 @@ public class UntilOne {
         int k = sc.nextInt();
         int count = 0;
 
-        while(n != 1) {
-            if(n % k == 0) {
-                n /= k;
-            } else {
-                n --;
+        while(true) {
+
+            int target = (n / k) * k; // k로 나눈 몫을 k로 곱한 수 = k로 나누어지는 수 (/연산은 나머지를 표시하지 않기에)
+            count += (n - target); // 나머지가 나옴, 해당 나머지를 1씩 빼는 행위 대신, 한번에 연산의 결과로 넣어버림
+            // 나머지를 일일이 1씩 빼기고 count++하기 = 나머지의 값을 count에 더하기가 성립하기 때문
+            n = target; // n은 k로 나누어지는 수가 됨
+
+            // N이 K보다 작을 경우 == 더 이상 나눌 수 없는 경우 -> 반복문 탈출
+            if(n < k) {
+                break;
             }
-            count ++;
+            n /= k; // n을 k로 나눈 값으로 업데이트
+            count ++; // 나누기 연산의 횟수를 증가
         }
+
+        // 남은 수에 대해 1씩 뺀 연산 횟수를 더해주기
+        count += (n - 1);
+
         System.out.println(count);
     }
-
 }
