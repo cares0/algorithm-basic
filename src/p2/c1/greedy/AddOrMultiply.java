9332b7cf0fc7849a78c098fd2bbc0a6726cdd2b6
cares0 <cares00000@gmail.com>
2022-04-24 21:54:41 +0900

    2-2. 그리디 - 더하기 혹은 곱하기(개선)
    알고리즘 개선

--- src/p2/c1/greedy/AddOrMultiply.java
+++ src/p2/c1/greedy/AddOrMultiply.java
@@ -16,17 +16,17 @@ public class AddOrMultiply {
         Scanner sc = new Scanner(System.in);
 
         String numbers = sc.nextLine();
-        int result = Character.getNumericValue(numbers.charAt(0));
+        int result = numbers.charAt(0) - '0';
+        // 0에 해당하는 아스키코드 값을 빼서도 int형으로 변환 가능
 
         for(int i=0; i<numbers.length() - 1; i++) {
-            int back = Character.getNumericValue(numbers.charAt(i+1));
-            if(result * back >= result + back) {
-                result *= back;
+            int number = numbers.charAt(i+1) - '0';
+            if(result <= 1 || result <= 1) {
+                result += number;
             } else {
-                result += back;
+                result *= number;
             }
         }
-
         System.out.println(result);
     }
 }
