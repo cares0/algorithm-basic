8228b7bfb9d0fa5032d01d51aa75e217004d93bd
cares0 <cares00000@gmail.com>
2022-04-25 19:28:57 +0900

    3-1. 구현 - 상하좌우(교재풀이)

--- src/p3/implementation/FourDirections.java
+++ src/p3/implementation/FourDirections.java
@@ -10,38 +10,30 @@ public class FourDirections {
 
         int n = sc.nextInt(); // 공간 크기 (n * n)
         sc.nextLine();
-        String[] plan = sc.nextLine().split(" "); 
+        String[] plans = sc.nextLine().split(" ");
         // 한 줄 전체를 받아오는데, " "를 기준으로 쪼개서 String[]로 만든다.
 
-        int x = 1;
-        int y = 1;
+        int x = 1, y = 1;
 
-        for (int i = 0; i < plan.length; i++) {
-            switch (plan[i]) {
-                case "U":
-                    if (x <= 1) {
-                        continue;
-                    }
-                    x --;
-                    break;
-                case "D":
-                    if (x >= n) {
-                        continue;
-                    }
-                    x ++;
-                    break;
-                case "L":
-                    if (y <= 1) {
-                        continue;
-                    }
-                    y --;
-                    break;
-                default:
-                    if (y >= n) {
-                        continue;
-                    }
-                    y ++;
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
+        char[] moveTypes = {'L', 'R', 'U', 'D'};
+
+        for (int i = 0; i < plans.length; i++) {
+            char plan = plans[i].charAt(0);
+            int nx = -1, ny = -1;
+            for (int j = 0; j < 4; j++) {
+                if (plan == moveTypes[j]) {
+                    // 총 이동할 값 계산
+                    nx = x + dx[j];
+                    ny = y + dy[j];
+                }
             }
+            if (nx < 1 || ny < 1 || nx > n || ny > n) {
+                continue;
+            }
+            x = nx;
+            y = ny;
         }
         System.out.println(x + " " + y);
     }
